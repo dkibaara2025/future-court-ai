2e1a9d97ea03918afd793b49631a01bf3200c2cc
dkibaara2025 <dkibaara@googlemail.com>
2026-07-30 20:01:40 +0300

    Prepare Cloudflare Pages deployment

--- src/app.js
+++ src/app.js
@@ -83,4 +83,4 @@ function bind() {
 }
 
 render();
-if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/public/sw.js').catch(() => undefined));
+if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => undefined));
